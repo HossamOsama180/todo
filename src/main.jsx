f8f6960171e1b6fd3f67cc8815797914c1a9d314
HossamOsama180 <http://hossamosamaz180@gmail.com>
2025-02-22 23:22:22 +0200

    Fix Redux folder name and import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import store from "./Redux/store.jsx"; // ✅ لاحظ الحرف الكبير
+// import store from "./redux/store.jsx"; // ✅ لاحظ الحرف الكبير
 import { Provider } from 'react-redux'
+import store from "./redux/store"; // ✅ استخدم الحروف الصغيرة
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
